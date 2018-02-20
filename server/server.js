d948bed59096c9bb6ed3f88db7164e6f5bd0c0c5
Lightyaer <mmnyed@gmail.com>
2018-02-21 00:02:25 +0530

    Fixed THE BUG and added GET /todos

--- server/server.js
+++ server/server.js
@@ -16,11 +16,21 @@ app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
     });
-    todo.save().then(() => {
+    todo.save().then((doc) => {
         res.status(200).send(doc);
     }, (e) => {
         res.status(400).send(e);
     })
+});
+
+app.get('/todos', (req, res) => {
+
+    Todo.find().then(todos => {
+        res.status(200).send({ todos })
+    }, (e) => {
+        res.status(400).send(e);
+    });
+
 });
 
 
